7bd87bae77b55fff3b08c6c0b2766bc9e16a10e7
Steve Alex <stevealexrs@protonmail.com>
2022-03-19 22:18:14 +0000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# SolidityBinding
+
+A description of this package.
---
+++ Sources/SolidityBinding/SolidityBinding.swift
@@ -0,0 +1,6 @@
+public struct SolidityBinding {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
