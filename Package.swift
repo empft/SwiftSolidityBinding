94339a535b262cc6a6ea79801982a6246485bd15
Steve Alex <stevealexrs@protonmail.com>
2022-03-25 00:02:25 +0000

    Renamed package

--- Package.swift
+++ Package.swift
@@ -4,15 +4,15 @@
 import PackageDescription
 
 let package = Package(
-    name: "SolidityBinding",
+    name: "SwiftSolidityBinding",
     platforms: [
         .macOS(.v11)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .executable(
-            name: "SolidityBinding",
-            targets: ["SolidityBinding"]),
+            name: "SwiftSolidityBinding",
+            targets: ["SwiftSolidityBinding"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -24,14 +24,14 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .executableTarget(
-            name: "SolidityBinding",
+            name: "SwiftSolidityBinding",
             dependencies: [
                 .product(name: "SwiftFormat", package: "swift-format"),
                 .product(name: "ArgumentParser", package: "swift-argument-parser"),
             ]
         ),
         .testTarget(
-            name: "SolidityBindingTests",
-            dependencies: ["SolidityBinding"]),
+            name: "SwiftSolidityBindingTests",
+            dependencies: ["SwiftSolidityBinding"]),
     ]
 )
